601e5490fc32391af964223ce429c110a78e664c
jeremie <jeremie-codes>
2025-04-02 22:56:33 +0100

    deploie visit

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ContactController;
 use App\Http\Middleware\LogVisit;
 
-Route::middleware([LogVisit::class])->group(function () {
-    Route::get('/', [HomeController::class, 'index'])->name('home');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/about', function () {
     return view('public.about');
 })->name('about');
 Route::get('/services/{service}', [HomeController::class, 'show'])->name('services.show');
-Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+Route::middleware([LogVisit::class])->group(function () {
+    Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+});
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
